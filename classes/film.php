2bf3ceaa32d4f03d4e794a990b3c774aa334995b
mctupane <79125468+TUPANE@users.noreply.github.com>
2021-08-27 16:35:30 +1100

    ajout fichier dans le dossier class

---
+++ classes/film.php
@@ -0,0 +1,9 @@
+<?php
+
+class Film { 
+    
+
+    // ici on va essayer de rajouter des variables et des fonctions, qui pourront par la suite être réutiliser dans un autre obljet.
+    // se référer ici à la Pierre GIRAUD
+}
+         
--- films.php
+++ films.php
@@ -8,9 +8,9 @@ $result = $laBase->query('SELECT * FROM sakila.film');
 
 foreach( $result->fetch_all(MYSQLI_ASSOC) as $obj)
 {
-   echo "<div style='color:blue' class='container'>
-   <label>".$obj["title"] ."</label>
-   <p>". $obj["description"] ."</p>
+   echo "<div style='color:red' class='container'>
+   <label>".$obj["title"]."</label>
+   <p>".$obj["description"]."</p>
   </div>";
 }
 
