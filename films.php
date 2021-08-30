c00f0187d58af735f9e9004c68462a797822e9a2
mctupane <79125468+TUPANE@users.noreply.github.com>
2021-08-31 08:00:07 +1100

    tableau

--- films.php
+++ films.php
@@ -4,13 +4,47 @@ require 'classes/db.php';
    
 $laBase = new Database;
 
-$result = $laBase->query('SELECT * FROM sakila.film');
+$result = $laBase->query('SELECT title, description, rental_date, return_date FROM sakila.film, sakila.rental LIMIT 1000');
+
+$disp =  'return_date';
+ if ($disp!= 'return_date') 
+ {
+    echo "oui";
+ }
+
+
+
+echo "
+  <h1 class='text-center'>LISTES DES FILMS CHEZ SAKILA </h1>
+<table border=2>
+  <thead>
+      <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Rental_date</th>
+          <th>Return_date</th>
+          <th>Disponibility</th>
+
+                 
+      </tr>
+  </thead>
+";
 
 foreach( $result->fetch_all(MYSQLI_ASSOC) as $obj)
 {
-   echo "<div style='color:red' class='container'>
-   <label>".$obj["title"]."</label>
-   <p>".$obj["description"]."</p>
-  </div>";
+   echo "
+          <tbody>
+              <tr>
+                <td>".$obj["title"]."</td>  
+                <td>".$obj["description"]."</td>
+                <td>".$obj["rental_date"]."</td>
+                <td>".$obj["return_date"]."</td>
+                
+              </tr>
+          </tbody>
+  ";
 }
 
+echo "
+</table> 
+";
